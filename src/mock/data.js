a7cc1d67446b188d9f68d829d1f62cbaa7d6b766
thobenayann <63640040+thobenayann@users.noreply.github.com>
2021-03-02 13:51:54 +0100

    add-new-project

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,7 +18,7 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: 'Après 10 ans passés dans le commerce et la vente de matériel industriel, à manager et atteindre des objectifs de performance et de satisfaction client, j\'ai choisi de donner un nouvel élan à ma carrière et de relever un nouveau défi: celui de devenir déveoppeur web !',
+  paragraphOne: 'Après 10 ans passés dans le commerce et la vente de matériel industriel, à manager et atteindre des objectifs de performance et de satisfaction client, j\'ai choisi de donner un nouvel élan à ma carrière et de relever un nouveau défi: celui de devenir développeur web !',
   paragraphTwo: 'Il s\'agit d\'un métier qui me passionne, qui me permet d\'apprendre tous les jours et souvent par moi même mais avec en plus, cette sensation que tout est possible pour peu que l\'on ai une connexion internet à portée de main. J\'ai été formé au sein de l\'école O\'Clock depuis le mois d\'Août et j\'ai pu réaliser un projet de fin d\'étude durant 1 mois en tant que Git Master et Développeur Front. Pour se faire, j\'ai choisi d\'apprendre à maîtriser React et Redux afin d\'optimiser toujours plus l\'expérience des utilisateurs.',
   paragraphThree: 'De nature curieux et très impliqué, je m\'intéresse à tout nouveau défi, je m\'adapte très facilement en équipe et j\'aime partager mon expérience, aussi petite soit elle ! Mon nouvel objectif est de trouver une entreprise qui m\'accueille pour un stage d\'1 an en alternance afin de parfaire mes connaissances de développeur Full Stack, j\'aimerais ainsi me préparer à passer le titre professionnel de "Concepteur Développeur d\'Application"',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
@@ -31,9 +31,18 @@ export const projectsData = [
     img: 'happy-news-website.png',
     title: 'Happy News',
     info: 'Projet de fin d\'étude au sein de l\'école O\'Clock',
-    info2: 'Tout d’abord, l’idée du projet est née du constat que beaucoup de PME comme les commerces de proximité (Producteurs locaux et artisan) n’ont pas les moyens d’investir dans une campagne de communication, pourtant à l’heure actuel, la visibilité de ces commerces est l’un des points cruciaux à leur survie ! Afin de répondre à cette problématique et renouer le lien entre ces 2 acteurs. Il a fallu imaginer et concevoir une plateforme de communication qui permettrait à n’importe quel petit commerce de pouvoir de créer un profil en quelques clics, consultable par tous les visiteurs du site et avec un moyen d’attirer leur attention...',
+    info2: 'Tout d’abord, l’idée du projet est née du constat que beaucoup de PME comme les commerces de proximité (Producteurs locaux et artisans) n’ont pas les moyens d’investir dans une campagne de communication, pourtant à l’heure actuelle, la visibilité de ces commerces est l’un des points cruciaux à leur survie ! Afin de répondre à cette problématique et renouer le lien entre ces 2 acteurs. Il a fallu imaginer et concevoir une plateforme de communication qui permettrait à n’importe quel petit commerce de pouvoir créer un profil en quelques clics, consultable par tous les visiteurs du site et avec un moyen d’attirer leur attention... Technologies utilisées : FRONT = React, Redux, Sass, Axios, Leaflet, Firebase ----- BACK = Node.js, Express, PostgreSQL, Bcrypt, JWT',
     url: 'https://happy-news.bluefoo.fr/',
     repo: 'https://github.com/O-clock-Lyra-JS/projet-happy-news', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'find-on-github.png',
+    title: 'Find on Github',
+    info: 'Projet personnel : site de recherche de repo Github',
+    info2: 'Projet réalisé en React avec Semantic UI en tant que librairie de composants ainsi que axios et l\'API de Github pour la recherche de repo sur github.com',
+    url: 'https://find-on-github.netlify.app/',
+    repo: 'https://github.com/thobenayann/find-on-github', // if no repo, the button will not show up
   },
     {
     id: nanoid(),
